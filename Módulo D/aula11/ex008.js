b31ac909e7768e04f2bdaea44cdbe3d27c033aae
Vitor Ramos <ramossvitor@gmail.com>
2021-07-14 18:01:24 -0300

    Estudando

---
+++ Módulo D/aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+
+console.log(`A sua velocidade é ${vel}km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida, portanto, foi multado.`)
+}
+console.log(`Diriga sempre usando o cinto de segurança!`)
