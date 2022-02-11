e0a7f56fd3bf990d41526b51e6ff2361020af6ed
Vitor Ramos <ramossvitor@gmail.com>
2022-02-10 21:52:14 -0300

    área adicionada

---
+++ Area Livre para Código/js/script.js
@@ -0,0 +1,10 @@
+var letra = "a"
+
+switch(letra){
+    case "a": console.log("Vogal"); break;
+    case "e": console.log("Vogal"); break;
+    case "i": console.log("Vogal"); break;
+    case "o": console.log("Vogal"); break;
+    case "u": console.log("Vogal"); break;
+    default: console.log("Consoante");
+}
---
+++ Area Livre para Código2/js/script.js
@@ -0,0 +1,15 @@
+for(var i = 0; i < 10; i++){
+    var estrela = '';
+    for(var j = 0; j < i; j++){
+        estrela += '*';
+    }
+    console.log(estrela)
+}
+
+for(var i = 10; i > 0; i--){
+    var estrela = '';
+    for(var j = 0; j < i; j++){
+        estrela += '*';
+    }
+    console.log(estrela)
+}
